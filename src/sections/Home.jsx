6b182baff4d23a5e52bfe160867fa341830f8daa
Hritik-Kumar-dev <hritikkumar.dev77@gmail.com>
2025-09-20 03:00:28 +0530

    updat

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -2,10 +2,10 @@ const Home = ({ scrollTo, contactRef }) => (
   <section className="h-screen  w-screen flex  justify-center   text-4xl font-bold">
 
 
-    <div className=' w-[100%] h-[100%] p-9 relative z-20 flex flex-col items-center justify-around'>
-
+    <div className=' w-[100%] h-[100%] p-9 relative z-20 flex flex-col  justify-around'>
+<h1 className="bitcount-grid-double-dotted text-8xl text-gray-300">WELCOME...</h1>
       <h1 className='w-[70%]  text-5xl text-gray-300  poetsen-one-regular'>Hii , <br /> I'm  <span className="bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent">Hritik</span>, <br /><span className="bg-gradient-to-r from-fuchsia-600 to-pink-600 bg-clip-text text-transparent ">Web Developer</span></h1>
-      <p className='text-2xl  audiowide-regular '>creative web developer...</p>
+      <p className='text-2xl  audiowide-regular '>creative web developer...    WELCOME</p>
 
     </div>
 
